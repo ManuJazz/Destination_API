4cc53bf51b9cfd5b3dd6e726b430e855ad650fe3
ManuJazz <mjesusaz@alumnos.unex.es>
2018-11-23 16:23:34 +0000

    API implementation completed!

--- src/main/java/io/swagger/api/impl/DestinationApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/DestinationApiServiceImpl.java
@@ -8,6 +8,7 @@ import io.swagger.model.DestinationItem;
 
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
@@ -23,34 +24,60 @@ import javax.validation.constraints.*;
 
 public class DestinationApiServiceImpl extends DestinationApiService {
     
+    List<DestinationItem> destination_list = new ArrayList<DestinationItem>();
+    
     @Override
     public Response adddestination(DestinationItem body, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        DestinationItem destination_item = new DestinationItem();
+        destination_item.setDenomination(body.getDenomination());
+        destination_item.setIdDestination(body.getIdDestination());
+        destination_item.setXCoordinate(body.getXCoordinate());
+        destination_item.setYCoordinate(body.getYCoordinate());
+        destination_list.add(destination_item);
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Destination added!")).build();
     }
     
     @Override
     public Response deleteDestination(Long destinationId, String apiKey, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        for(int index=0;index<destination_list.size();index++){
+            if(destination_list.get(index).getIdDestination().equals(destinationId)){
+                destination_list.remove(index);
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Destination deleted!")).build();
+            }
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.ERROR, "Destination not found!")).build();
     }
     
     @Override
     public Response getDestinationById(Long destinationId, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        for(int index=0;index<destination_list.size();index++){
+            if(destination_list.get(index).getIdDestination().equals(destinationId)){
+                DestinationItem destination_found = destination_list.get(index);
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, destination_found.toString())).build();
+            }
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.ERROR, "Destination not found!")).build();
     }
     
     @Override
     public Response getalldestinations(SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, destination_list.toString())).build();
     }
     
     @Override
     public Response updateDestination(DestinationItem body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        for(int index=0;index<destination_list.size();index++){
+            if(destination_list.get(index).getIdDestination().equals(body.getIdDestination())){
+                destination_list.get(index).setDenomination(body.getDenomination());
+                destination_list.get(index).setIdDestination(body.getIdDestination());
+                destination_list.get(index).setXCoordinate(body.getXCoordinate());
+                destination_list.get(index).setYCoordinate(body.getYCoordinate());
+                return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, destination_list.get(index).toString())).build();
+            }
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.ERROR, "Destination not found!")).build();
     }
     
 }
